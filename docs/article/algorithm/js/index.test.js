f05870281a7d1ea1897a1fbaa061a5127dd7f742
kkkkh <13693256971@163.com>
2025-02-07 10:59:30 +0800

    Update index.test.js

--- docs/article/algorithm/js/index.test.js
+++ docs/article/algorithm/js/index.test.js
@@ -1,5 +1,5 @@
-// import { childArrIsSame } from './index.js'
-const {childArrIsSame} = require('./index.js');
+import { expect, test } from 'vitest'
+import {childArrIsSame,hasDuplicates} from './index.js'
 
 test('childArrIsSame true', () => {
   const arr = [
@@ -17,4 +17,14 @@ test('childArrIsSame false', () => {
     [1, 2, 3],
   ]
   expect(childArrIsSame(arr)).toEqual(arr[0].sort())
+})
+
+test('hasDuplicates false', () => {
+  const arr = ['a', 'b', 'c', 'd']
+  expect(hasDuplicates(arr)).toEqual(false)
+})
+
+test('hasDuplicates true', () => {
+  const arr = ['a', 'b', 'c', 'd', 'a']
+  expect(hasDuplicates(arr)).toEqual(true)
 })
