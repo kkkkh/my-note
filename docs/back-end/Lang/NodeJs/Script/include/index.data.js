9f135417f55f6dffd2c8b7f5c50cf595bb6d2cdd
kkkkh <13693256971@163.com>
2025-03-03 18:07:59 +0800

    Update index.data.js

--- docs/back-end/Lang/NodeJs/Script/include/index.data.js
+++ docs/back-end/Lang/NodeJs/Script/include/index.data.js
@@ -1,28 +1,27 @@
-// import fs from 'node:fs'
 export default {
   // 相对路径
   // watch: ['../../../../../front-end/DesignPatterns/md/*.md'],
   // 根目录路径
   // watch: ['docs/front-end/javaScript/ES6/md/*.md'],
-  // watch: ['docs/front-end/DesignPatterns/md/*.md'],
-  watch: ['docs/back-end/Lang/NodeJs/Book/note/**/*.md'],
+  watch: ['docs/front-end/DesignPatterns/md/*.md'],
+  // watch: ['docs/back-end/Lang/NodeJs/Book/note/**/*.md'],
   load(watchedFiles) {
-    // console.log(watchedFiles)
     const files = watchedFiles.sort((pre,next)=>{
-      // console.log(pre.match(/(\d+)[、-]/))
-      // console.log(next.match(/(\d+)[、-]/))
       const preNum = pre.match(/(\d+)[、-]?/)
       const nextNum = next.match(/(\d+)[、-]?/)
-      // console.log("preNum",preNum)
-      // console.log("nextNum",nextNum)
       if(preNum === null || nextNum===null)return 0
       return preNum[1] - nextNum[1]
-    }).map(item => {
-      // const path = item.match(/\/([^\/]+)$/)[1]
-      // return  `<!--@include: ./md/${path}-->`.replaceAll(/\\/g,'/')
-      return  `<!--@include: ${item}-->`.replaceAll(/\\/g,'/')
     })
-    // fs.writeFileSync('./index.md',files.join("\n"))
+    // .map(item => {
+    // // 相对路径 => ./md/${文件名}
+    //   const path = item.match(/\/([^\/]+)$/)[1]
+    //   return  `<!--@include: ./md/${path}-->`.replaceAll(/\\/g,'/')
+    // })
+    .map(item => {
+    // 根路径 => ./md/${文件名}
+      const path = item.replace("docs","@")
+      return  `<!--@include: ${path}-->`.replaceAll(/\\/g,'/')
+    })
     return files
-  }
+  },
 }
