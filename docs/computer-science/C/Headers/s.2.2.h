6aaecbe9b8bc02128a23089c1d3390f3518f61e3
kkkkh <13693256971@163.com>
2025-04-14 13:42:08 +0800

    Create s.2.2.h

---
+++ docs/computer-science/C/Headers/s.2.2.h
@@ -0,0 +1,9 @@
+#ifndef __S_2_2_
+#define __S_2_2_
+int pointerMain(void);
+void triplePointer(int *pointerOnNumber);
+int pointer1Main();
+int pointer2Main();
+int pointerTimeMain();
+void transformMinutes(int *hours, int *minutes);
+#endif
