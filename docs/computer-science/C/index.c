0a550ba5b7141e82d029fb9fd931ef1134f53ad2
kkkkh <13693256971@163.com>
2025-04-18 11:42:16 +0800

    Update index.c

--- docs/computer-science/C/index.c
+++ docs/computer-science/C/index.c
@@ -4,34 +4,33 @@
     stdio.h库文件
 */
 #include <stdio.h>
-#include "./Headers/s1g.h"
-#include "./Headers/s2.1.h"
 #include "./Headers/s1.h"
-
-int main(void) {
-    // 基础
-    int number;
-    /*
-        printf 也是一个函数，定义在 stdio 库中
-        特殊字符：第一个字符是反斜杠（\），第二个字符是一个数字或者一个字母。
-        \n ：换行符 （作用类似按回车键）。
-    */
-    printf("input:\n"); //
-    scanf("%d", &number);
-    if((number > 1) && (number <= 100)) {
-        printf("Small\n");
-    } else if((number > 101) && (number <= 200)) {
-        printf("Middle\n");
-    } else {
-        printf("Large\n");
-    }
-
+#include "./Headers/s1g.h"
+#include "./Headers/s2.h"
+/*
+    int main(int argc, char *argv[])
+    char *argv[]一个指向字符串的指针数组
+    数组中的每个元素 argv[i] 都是一个 char * 类型的指针
+*/
+int main(int argc, char *argv[]) {
     // s1
-    s1Main();
+    // s1Main();
     // s1g
-    guessNumber();
+    // guessNumber();
     // s2.1
-    staticMain();
+    // staticMain();
+    // s2.2
+    // pMain();
+    // s2.3
+    // arrayMain();
+    // s2.4
+    // stringMain();
+    // s2.6
+    // s2_6_main();
+    // s2.7
+    // s2_7_main();
+    // s2.8
+    s2_8_main();
     /*
         大致就是表示函数结束了，并且返回 0 值。
         0 表示一切正常结束，其他的值表示异常
