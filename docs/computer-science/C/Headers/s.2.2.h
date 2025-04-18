2de9f36948b7cab4d8c8e7ed26afdb51df7cef46
kkkkh <13693256971@163.com>
2025-04-18 11:40:20 +0800

    Delete docs/computer-science/C/Headers/s.2.2.h

--- docs/computer-science/C/Headers/s.2.2.h
+++
@@ -1,9 +0,0 @@
-#ifndef __S_2_2_
-#define __S_2_2_
-int pointerMain(void);
-void triplePointer(int *pointerOnNumber);
-int pointer1Main();
-int pointer2Main();
-int pointerTimeMain();
-void transformMinutes(int *hours, int *minutes);
-#endif
