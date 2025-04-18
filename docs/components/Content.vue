6e5b4934cd55bdc43276bc7608f8ce7c219af528
kkkkh <13693256971@163.com>
2025-04-18 13:55:52 +0800

    Create Content.vue

---
+++ docs/components/Content.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+interface Props {
+  posts?: {url:string,title:string}[]
+}
+const { posts = [] } = defineProps<Props>()
+import { withBase } from 'vitepress'
+</script>
+<template>
+  <ul>
+    <li v-for="post of posts">
+      <a :href="withBase(post.url)">{{ post.title }}</a>
+    </li>
+  </ul>
+</template>
