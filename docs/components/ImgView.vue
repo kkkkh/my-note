c672ff6fc329ec96bf477372bb543f20fa516840
kkkkh <13693256971@163.com>
2025-04-24 16:46:39 +0800

    Create ImgView.vue

---
+++ docs/components/ImgView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="img-view" v-viewer>
+      <img v-for="src in images" :key="src" :src="src">
+    </div>
+  </div>
+</template>
+<script setup>
+  defineOptions({
+    name: 'ImgView'
+  })
+  import 'viewerjs/dist/viewer.css'
+  const props = defineProps({
+    images: {
+      type: Array,
+      default: () => []
+    }
+  })
+  // const images = ref( [
+  //   "https://picsum.photos/200/200",
+  //   "https://picsum.photos/300/200",
+  //   "https://picsum.photos/250/200"
+  // ])
+</script>
+<style lang="scss" scoped>
+.img-view {
+  img {
+    width: 100%;
+  }
+}
+</style>
