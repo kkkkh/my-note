a195d1c501583e6193d502ee5f87244cb2999c75
kkkkh <13693256971@163.com>
2025-04-24 17:01:53 +0800

    Add files via upload

---
+++ docs/components/RenderHtml/ShadowDom.vue
@@ -0,0 +1,17 @@
+<template>
+  <div ref="shadowHost"></div>
+</template>
+<script setup>
+import { ref, onMounted } from 'vue';
+const props = defineProps({
+  html: {
+    type: String,
+    required: true
+  }
+})
+const shadowHost = ref(null);
+onMounted(() => {
+  const shadow = shadowHost.value.attachShadow({ mode: 'open' });
+  shadow.innerHTML = props.html;
+});
+</script>
