2348aec57b3c14a6a14334bf2c0f91f75bc80f89
kkkkh <13693256971@163.com>
2025-04-24 17:01:20 +0800

    Create Fetch.vue

---
+++ docs/components/RenderHtml/Fetch.vue
@@ -0,0 +1,26 @@
+<template>
+  <div v-html="htmlContent"></div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const htmlContent = ref('');
+const props = defineProps({
+  url: {
+    type: String,
+    required: true
+  }
+})
+onMounted(async () => {
+  try {
+    const response = await fetch(props.url);
+    // debugger
+    htmlContent.value = await response.text();
+    console.log(htmlContent.value)
+  } catch (error) {
+    console.error('Error fetching HTML file:', error);
+    htmlContent.value = '<p>Failed to load HTML content.</p>';
+  }
+});
+</script>
