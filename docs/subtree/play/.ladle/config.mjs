6c73c5b52cfbf039b05d7151d8c9a15a586eee48
kkkkh <13693256971@163.com>
2025-05-21 16:49:05 +0800

    Delete docs/subtree/play directory

--- docs/subtree/play/.ladle/config.mjs
+++
@@ -1,6 +0,0 @@
-/** @type {import('@ladle/react').UserConfig} */
-export default {
-  outDir: "./docs",
-  base: "/play/",
-  stories: "./packages/**/src/**/*.stories.{js,jsx,ts,tsx,mdx}",
-};
--- docs/subtree/play/packages/react/src/react19/index.stories.tsx
+++
@@ -1,7 +0,0 @@
-import type { StoryDefault } from "@ladle/react";
-export default {
-  title: "React / React19",
-} satisfies StoryDefault;
-export { default as Action } from "./action/Index"
-export { default as Use } from "./use/Index"
-export { default as Ref19 } from "./ref/Index"
