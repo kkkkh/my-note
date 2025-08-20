0d73573598d0c0bd124685466b2a31eaf9ed396c
zhanghaotian <haotian.zhang5@transsion.com>
2025-08-20 14:12:52 +0800

    fix: hd 20250820 随笔

---
+++ docs/components/Idea.vue
@@ -0,0 +1,12 @@
+<template>
+  <span :style="{ color: color, backgroundColor: backgroundColor }">
+    <slot />
+  </span>
+</template>
+
+<script setup>
+const {color = "#EFE4D2",backgroundColor = "#254D70"} = defineProps({
+  color: String,
+  backgroundColor: String
+})
+</script>
